c1f7995270e095af54701b80740a26e7bc3254dd
akashgurunagoudar <akashgurunagoudar@gmail.com>
2025-10-06 15:31:23 +0530

    Update si.py

--- si.py
+++ si.py
@@ -1,6 +1,16 @@
-p=int(input("Enter principal amount:"))
-r=int(input("Enter rate of intrest:"))
-t=int(input("Enter time period:"))
+p=int(input("Enter the principal amount:"))
+t=int(input("Enter the time taken:"))
+r=int(input("Enter rate of interest:"))
+n = int(input("Enter the number of times interest is compounded per year: "))
+
 si=(p*t*r)/100
 
-print("Simple intrest:",si)
+print("Simple interest is:",si)
+
+
+amount = p * (1 + r / (100 * n)) ** (n * t)
+compound_interest = amount - p
+
+
+print(f"\nCompound Interest = {compound_interest:.2f}")
+print(f"Total Amount after {t} years = {amount:.2f}")
